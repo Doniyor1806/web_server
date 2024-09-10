789725aa0318892f2ec848ede432a2236bcbffa1
Doniyor1806 <mydonis23@gmail.com>
2024-09-10 00:20:05 -0400

    servers added

--- resources.tf
+++ resources.tf
@@ -6,6 +6,22 @@ variable "prefix" {
   type    = string
   default = "project-aug-28-web-server" #changed
 }
+
+##############################################(added)
+variable "instance_count" {
+  type    = number
+  default = 3
+}
+
+locals {
+  instance_names = [for i in range(var.instance_count) : "${var.prefix}-ec2-${i + 1}"]
+}
+
+###############################################
+
+
+
+
 resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
   tags = {
@@ -35,7 +51,7 @@ resource "aws_route_table_association" "main" {
 }
 module "security-grp" {
   source  = "app.terraform.io/donis_cloud/security-grp/aws"
-  version = "1.0.0"
+  version = "2.0.0"
   vpc_id  = aws_vpc.main.id
   security_groups = {
     "web" = {
@@ -76,12 +92,22 @@ module "security-grp" {
   }
 }
 
+locals {
+  web_servers = [
+    "web-server-001", #"44.222.2.28"
+    "web-server-002", #"3.216.63.186"
+    "web-server-003"  #"44.214.243.175"
+  ]
+}
+
+
 resource "aws_instance" "server" {
+  for_each               = toset(local.web_servers)
   ami                    = "ami-0182f373e66f89c85"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   subnet_id              = aws_subnet.main.id
-  vpc_security_group_ids = [module.security-grp.my-security_gr_id["web"]]
+  vpc_security_group_ids = [module.security-grp.security_group_id["web"]]
 
   user_data = <<-EOF
                      #!/bin/bash
@@ -89,21 +115,29 @@ resource "aws_instance" "server" {
                      sudo yum install -y httpd
                      sudo systemctl start httpd.service
                      sudo systemctl enable httpd.service
-                     echo "<h1> Hello World from DS </h1>" | sudo tee /var/www/html/index.html
+                     echo "<h1> Hello World from DS ${each.key} </h1>" | sudo tee /var/www/html/index.html
   EOF
   tags = {
     Name = join("-", [var.prefix, "ec2"])
   }
-  lifecycle { ### this is for recreating resource and relaunch
+  lifecycle { ### this is for recreating resource and relaunching
     create_before_destroy = true
   }
 }
 
 resource "aws_eip" "instance_ip" {
-  instance = aws_instance.server.id
+  for_each = aws_instance.server
+  instance = each.value.id
   domain   = "vpc"
 
 }
-output "instance_public_ip" {
-  value = aws_eip.instance_ip.public_ip # Output the public IP of the Elastic IP
+# output "instance_public_ip" {
+#   value = aws_eip.instance_ip.public_ip # Output the public IP of the Elastic IP #### i need it bcause it's for only 1 instance
+# }
+
+################################## (added)
+output "instance_public_ips" {
+  value = { for instance, value in aws_eip.instance_ip : instance => value.public_ip }
 }
+
+##################################
